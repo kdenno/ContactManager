d320106a3107de5eb707a51ed371129a1dabe937
kdenno <kabuyedennis3@gmail.com>
2020-04-24 09:06:33 +0300

    configure concurrently to run both client and backend servers

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
 const contactsRoutes = require("./routes/contacts");
