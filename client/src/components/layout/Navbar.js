f021abbc891ca4cef2c466da028260a18f147e41
kdenno <kabuyedennis3@gmail.com>
2020-04-26 20:35:45 +0300

    update navbar links after auth

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,29 +1,45 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import AuthContext from "../../context/auth/authContext";
 
 const Navbar = ({ title, icon }) => {
+  const authcontext = useContext(AuthContext);
+  const { LogOut, isAuthenticated, user } = authcontext;
+  const onLogout = () => {
+    LogOut();
+  }
+
+  let thelinks;
+  if (isAuthenticated) {
+    thelinks = (
+      <Fragment>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <a href="#!" onClick={onLogout}>LogOut</a>
+        </li>
+      </Fragment>
+    );
+  } else {
+    thelinks = (
+      <Fragment>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+      </Fragment>
+    );
+  }
   return (
     <Fragment className="navbar">
-  
       <div className="navbar bg-primary">
         <i className={icon}></i> {title}
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-        </ul>
+        <ul>{thelinks}</ul>
       </div>
-
     </Fragment>
   );
 };
